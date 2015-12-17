84ce09dfb5e8f0febd09ccc862d04d27311bb187
Marin Takanov <mbtakanov@yahoo.com>
2015-12-17 23:22:07 +0200

    ContactUs and Share pages changes

--- Frontend/scripts/controllers/ContactUsController.js
+++ Frontend/scripts/controllers/ContactUsController.js
@@ -8,39 +8,38 @@ app.controller('ContactUsController',
 		$scope.invalidContent = false;
 
 		$scope.subjectChange = function () {
-			if ($scope.invalidUsername && $scope.reg.username && $scope.reg.username.length >= 4) {
-				$scope.invalidUsername = false;
+			if ($scope.invalidSubject && $scope.message.subject && $scope.message.subject.length >= 1) {
+				$scope.invalidSubject = false;
 			}
 		};
 
 		$scope.emailChange = function () {
-			if ($scope.invalidEmail && $scope.reg.email && $scope.reg.email.length > 0) {
+			if ($scope.invalidEmail && $scope.message.email && $scope.message.email.length > 0) {
 				$scope.invalidEmail = false;
 			}
 		};
 
 		$scope.contentChange = function () {
-			if ($scope.invalidPassword && $scope.reg.password && $scope.reg.password.length >= 4) {
-				$scope.invalidPassword = false;
+			if ($scope.invalidContent && $scope.message.content && $scope.message.content.length >= 4) {
+				$scope.invalidContent = false;
 			}
 		};
 
 		$scope.send = function () {
-			if (!$scope.reg.username || $scope.reg.username.length <= 4 || $scope.reg.username.indexOf(' ') !== -1) {
-				$scope.invalidUsername = true;
+			if (!$scope.message.subject || $scope.message.subject.length <= 1) {
+				$scope.invalidSubject = true;
 			}
 
-			if (!$scope.reg.email || $scope.reg.email.length <= 5) {
+			if (!$scope.message.email || $scope.message.email.length <= 5) {
 				$scope.invalidEmail = true;
 			}
 
-			if (!$scope.reg.password || $scope.reg.password.length < 4) {
-				$scope.invalidPassword = true;
+			if (!$scope.message.content || $scope.message.content.length < 4) {
+				$scope.invalidContent = true;
 			}
 
 
-			if (!$scope.invalidUsername && !$scope.invalidEmail && !$scope.invalidPassword && 
-				!$scope.invalidConfPassword && !$scope.invalidPasswords) {
+			if (!$scope.invalidSubject && !$scope.invalidEmail && !$scope.invalidContent) {
 				// Send message.
 			}
 		};
--- Frontend/scripts/controllers/ShareController.js
+++ Frontend/scripts/controllers/ShareController.js
@@ -2,6 +2,46 @@
 
 app.controller('ShareController', 
 	['$scope', function ($scope) {
-		
+		$scope.article = {};
+		$scope.invalidSubject = false;
+		$scope.invalidEmail = false;
+		$scope.invalidContent = false;
+
+		$scope.subjectChange = function () {
+			if ($scope.invalidSubject && $scope.article.subject && $scope.article.subject.length >= 1) {
+				$scope.invalidSubject = false;
+			}
+		};
+
+		$scope.emailChange = function () {
+			if ($scope.invalidEmail && $scope.article.email && $scope.article.email.length > 0) {
+				$scope.invalidEmail = false;
+			}
+		};
+
+		$scope.contentChange = function () {
+			if ($scope.invalidContent && $scope.article.content && $scope.article.content.length >= 4) {
+				$scope.invalidContent = false;
+			}
+		};
+
+		$scope.send = function () {
+			if (!$scope.article.subject || $scope.article.subject.length < 1) {
+				$scope.invalidSubject = true;
+			}
+
+			if (!$scope.article.email || $scope.article.email.length <= 5) {
+				$scope.invalidEmail = true;
+			}
+
+			if (!$scope.article.content || $scope.article.content.length < 4) {
+				$scope.invalidContent = true;
+			}
+
+
+			if (!$scope.invalidSubject && !$scope.invalidEmail && !$scope.invalidContent) {
+				// Send article.
+			}
+		};
 	}]
 );
