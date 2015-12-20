df910c51f375e335cd188d98aa2919d49ffaf9d5
Ivan Zdravkov <IvanZDravkovBG@gmail.com>
2015-12-20 11:25:43 +0200

    Custom CustomOAuthProvider implemented. Sensitive Info removed from Web.config.

--- Backend/WebServices/Controllers/AccountsController.cs
+++ Backend/WebServices/Controllers/AccountsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.DataHandler.Encoder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
---
+++ Backend/WebServices/Providers/CustomJwtFormat.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+using Thinktecture.IdentityModel.Tokens;
+
+namespace WebServices.Providers
+{
+    public class CustomJwtFormat : ISecureDataFormat<AuthenticationTicket>
+    {
+
+        private readonly string issuer = string.Empty;
+
+        public CustomJwtFormat(string issuer)
+        {
+            this.issuer = issuer;
+        }
+
+        public string Protect(AuthenticationTicket data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var issued = data.Properties.IssuedUtc;
+            var expires = data.Properties.ExpiresUtc;
+
+            var token = new JwtSecurityToken(this.issuer,  ConfigurationManager.AppSettings["audienceId"], 
+                data.Identity.Claims, issued.Value.UtcDateTime, expires.Value.UtcDateTime,
+                new HmacSigningCredentials(TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["audienceSecret"])));
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = handler.WriteToken(token);
+
+            return jwt;
+        }
+
+        public AuthenticationTicket Unprotect(string protectedText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
