6f7751be0202f967008649c859879fc927551665
Ivan Zdravkov <IvanZDravkovBG@gmail.com>
2015-12-29 22:33:30 +0200

    Cross Origin and application/x-form fixes. Token now getting successfully.

--- Frontend/scripts/controllers/LoginController.js
+++ Frontend/scripts/controllers/LoginController.js
@@ -36,21 +36,21 @@ app.controller('LoginController',
 					password: $scope.password
 				};
 
-				var token = {
+				/*var token = {
 					"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiI2MzNkMDNlNS04NmZiLTRiNjEtOWZlYy0yNWZmZmZmNTM0YjciLCJ1bmlxdWVfbmFtZSI6ImRvaXR5b3Vyc2VsZi53ZWJtYWlsQGdtYWlsLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYWNjZXNzY29udHJvbHNlcnZpY2UvMjAxMC8wNy9jbGFpbXMvaWRlbnRpdHlwcm92aWRlciI6IkFTUC5ORVQgSWRlbnRpdHkiLCJBc3BOZXQuSWRlbnRpdHkuU2VjdXJpdHlTdGFtcCI6ImNjZDBjMDgzLTMyZDQtNGVjMC04MjRjLTI0Njc3OTdjN2RlYiIsInJvbGUiOlsiVXNlciIsIkFkbWluaXN0cmF0b3IiXSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDoxMzQ0NyIsImF1ZCI6IjYwMzlmNmUxMjNiNjQ2YTc5OTEzY2JmMjc2NmMwNWEyIiwiZXhwIjoxNDUxMjE5MjMwLCJuYmYiOjE0NTExMzI4MzB9.bx61rCYCKbEhI8YF0TVu16umwH72BtW0HOIZVZf3o9o",
 					"token_type": "bearer",
 					"isAdmin": true,
 					"expires_in": 86399,
 					"username": user.username
-				};
-				localStorage.setItem('user', JSON.stringify(token));
-				$rootScope.isLoggedIn = true;
-				$rootScope.isAdmin = true;
-				$rootScope.username = user.username;
-				$location.path('/');
-				// userService.loginUser(user).then(function (response) {
-					// $location.path('/');
-				// });
+				};*/
+				//localStorage.setItem('user', JSON.stringify(token));
+				//$rootScope.isLoggedIn = true;
+				//$rootScope.isAdmin = true;
+				//$rootScope.username = user.username;
+				//$location.path('/');
+				userService.loginUser(user).then(function (response) {
+					$location.path('/');
+				});
 			}
 		};
 	}]
