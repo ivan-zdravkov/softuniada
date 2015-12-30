36a3c5a28ce8c0e41afe4ed84bb996e7e11c0382
Ivan Zdravkov <IvanZDravkovBG@gmail.com>
2015-12-30 15:32:50 +0200

    DAL Setup. Entities Extended Audit Implemented. SoC.

---
+++ Backend/DAL/SoftuniadaDAL.cs
@@ -0,0 +1,33 @@
+﻿using DAL.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SoftuniadaDAL
+    {
+        private ExtendedSoftuniadaEntities db;
+
+        protected ExtendedSoftuniadaEntities DB
+        {
+            get
+            {
+                return this.db;
+            }
+        }
+
+        public SoftuniadaDAL(string userId)
+        {
+            db = new ExtendedSoftuniadaEntities(userId);
+        }
+
+        // Use this constructor for unit tests.
+        public SoftuniadaDAL(ExtendedSoftuniadaEntities extendedSoftuniadaEntities)
+        {
+            db = extendedSoftuniadaEntities;
+        }
+    }
+}
